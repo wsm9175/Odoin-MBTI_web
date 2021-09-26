839eb850d8d7380a2115a49a29e7ea7147e8595e
wsm9175 <76678911+wsm9175@users.noreply.github.com>
2021-09-27 00:44:24 +0900

    first commit

---
+++ js/data.js
@@ -0,0 +1,17 @@
+//qna 리스트 생성
+const qnaList  = [
+      {
+            q: '1. 유진이와 나랑 사귄 날짜는?' ,
+            a: [
+                  { answer : 'a. 4월6일이지롱', type: [ 'ENTP', 'ESFJ' ] },
+                  { answer : 'b. 4월16일인가??', type: ['INFP, INTP'] }
+            ]
+      },
+      {
+            q: '2. 유진이와 나랑 200일 날짜는?' ,
+            a: [
+                  { answer : 'a. 10월22일이지롱', type: [ 'ENTP', 'ESFJ' ] },
+                  { answer : 'b. 10월16일인가??', type: ['INFP, INTP'] }
+            ]
+      }
+];
